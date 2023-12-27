df7f9b7ff7082495685f0ca428b92386e087e5ee
tephyxp <lindacespedesg@gmail.com>
2023-12-27 21:36:34 +0100

    dependencias creadas

---
+++ src/app/characters/page.jsx
@@ -0,0 +1,6 @@
+export default function Characters(){
+    return (<section>
+        <h1 className="text-red-400">Sección Characters</h1>
+    </section>)
+
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function (){
+    return (<>
+    <h1 className="bg-black text-white">Soy un header</h1>
+    </>)
+}
